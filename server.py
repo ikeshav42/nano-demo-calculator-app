6ec4c2c271c7c8ea4c5f8e83269d6f963c0a11e1
Keshav Sundararaman <ikeshav42@gmail.com>
2023-08-15 17:54:30 +0530

    Added the functionalities

--- server.py
+++ server.py
@@ -1,19 +1,33 @@
-from flask import Flask
+
+from flask import Flask , request, jsonify
+# import requests
+# import jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    f = data['first']
+    s = data['second']
+
+    result = f + s
+    return jsonify({"result": result}), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    f = data['first']
+    s = data['second']
+
+    result = f - s
+
+    return jsonify({'result': result}), 200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
